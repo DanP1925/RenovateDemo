079a655b69714fbcd963ae940161e0ab31315191
Daniel Peñaloza <DanP1925@gmail.com>
2025-02-01 00:25:35 +0100

    first commit

---
+++ app/src/main/java/com/danp1925/renovatedemo/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.danp1925.renovatedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.danp1925.renovatedemo.ui.theme.RenovateDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            RenovateDemoTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AsyncImage(
+                model = "https://media.licdn.com/dms/image/v2/C4E0BAQFXF5Cde9RWig/company-logo_200_200/company-logo_200_200/0/1630588974580?e=1746662400&v=beta&t=jyIP3w_Igf_H2bvPgnTXz4mqz0iCbMG_tx7siLlKp5U",
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth().padding(24.dp)
+            )
+            Text(
+                text = "Hola Android Dev Peru",
+                style = TextStyle(fontSize = 32.sp)
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun MainPreview() {
+    RenovateDemoTheme {
+        MainScreen()
+    }
+}
